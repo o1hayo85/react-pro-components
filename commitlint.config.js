f431e3384dd248f8c51624065e4a0d39eb465aa1
joey <joey@egenie.cn>
2021-04-07 16:37:57 +0800

    feat(react-scripts): 解决css前缀自动被去掉。统一换行符格式,暂时在项目不更新

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,15 +1,1 @@
-module.exports = {
-  extends: ['@commitlint/config-conventional'],
-  rules: {
-    'type-case': [
-      2,
-      'always',
-      'camel-case',
-    ],
-    'scope-case': [
-      1,
-      'always',
-      'camel-case',
-    ],
-  },
-};
+module.exports = require('./packages/egenie-code-style').commitlint;
