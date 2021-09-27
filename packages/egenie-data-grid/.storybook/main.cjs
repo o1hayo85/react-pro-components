8f208e5978131ef77ba13ecebf9c0c06c60845ce
lynshir <lynshir@egenie.cn>
2021-09-27 16:10:40 +0800

    feat: 添加storybook

---
+++ packages/egenie-data-grid/.storybook/main.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  stories: ['../stories/**/*.story.*'],
+  reactOptions: {
+    fastRefresh: true,
+    strictMode: true
+  },
+  typescript: {
+    reactDocgen: false
+  }
+};
--- packages/egenie-data-grid/src/DataGrid.tsx
+++ packages/egenie-data-grid/src/DataGrid.tsx
@@ -69,6 +69,7 @@ type DefaultColumnOptions<R, SR> = Pick<
   'formatter' | 'minWidth' | 'resizable' | 'sortable'
 >;
 
+const globalThis = window.globalThis || window;
 // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
 const body = globalThis.document?.body;
 
