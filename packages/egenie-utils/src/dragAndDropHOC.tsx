1898134c183f241f6d81f4f53c9cbe4efaf55c41
joey <joey@egenie.cn>
2022-08-02 17:15:04 +0800

    feat(egenie-common): 去掉查询方案折叠属性

--- packages/egenie-utils/src/dragAndDropHOC.tsx
+++ packages/egenie-utils/src/dragAndDropHOC.tsx
@@ -2,8 +2,8 @@ import React, { useRef } from 'react';
 import { DndProvider, createDndContext } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 
+const RNDContext = createDndContext(HTML5Backend);
 export const DragAndDropHOC: React.FC = (props) => {
-  const RNDContext = React.useMemo(() => createDndContext(HTML5Backend), []);
   const manager = useRef(RNDContext);
   return (
     <DndProvider manager={manager.current.dragDropManager}>
