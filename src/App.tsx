8639b92f966a31e42f0094faf7b31bfd1bf1bcdd
yangxin <yangxin@totodi.com>
2023-02-06 17:42:02 +0800

    feat: 基础button样式

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
 import React from 'react';
+import Button, { ButtonSize, ButtonType } from './components/Button/button';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <Button>
+          hello
+        </Button>
+        <Button
+          btnType={ButtonType.Primary}
+          size={ButtonSize.Large}
+          disabled
+        >
+          hello
+        </Button>
+        <Button
+          btnType={ButtonType.Link}
+          href="http://www.baidu.com"
+          disabled
+        >
+          hello
+        </Button>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
--- src/components/Button/button.tsx
+++ src/components/Button/button.tsx
@@ -1,1 +1,68 @@
-export {}
+import React, { FC } from 'react';
+import classNames from 'classnames';
+
+export enum ButtonSize {
+  Large = 'lg',
+  Small = 'sm',
+}
+
+export enum ButtonType {
+  Primary = 'primary',
+  Default = 'default',
+  Danger = 'danger',
+  Link = 'link'
+}
+
+export interface ButtonProps {
+  className?: string;
+  disabled?: boolean;
+  size?: ButtonSize;
+  btnType?: ButtonType;
+  href?: string;
+  children: React.ReactNode;
+}
+
+const Button: FC<ButtonProps> = (props) => {
+  const {
+    btnType,
+    disabled,
+    size,
+    children,
+    href,
+    className,
+  } = props;
+
+  const classes = classNames('btn', {
+    [`btn-${btnType}`]: btnType,
+    [`btn-${size}`]: size,
+    'disabled': (btnType === ButtonType.Link) && disabled,
+    [`${className}`]: className,
+  });
+
+  if(btnType === ButtonType.Link ) {
+    return (
+      <a
+      className={classes}
+      href={href}
+    >
+      {children}
+    </a>
+    )
+  } else {
+    return (
+      <button
+        className={classes}
+        disabled={disabled}
+      >
+        {children}
+      </button>
+    )
+  }
+};
+
+Button.defaultProps = {
+  disabled: false,
+  btnType: ButtonType.Default
+}
+
+export default Button;
